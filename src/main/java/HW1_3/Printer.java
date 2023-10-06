4a84d8fd8a34d8999f3a41952433ecb03b20ce15
Tbolotova <secret048@yandex.ru>
2023-10-06 03:47:28 +0300

    all changes

--- src/main/java/HW1_3/Printer.java
+++ src/main/java/HW1_3/Printer.java
@@ -1,2 +1,24 @@
-package HW1_3;public class Printer {
+package HW1_3;
+
+public class Printer{
+    public static void main(String[] args) {
+        int a = 0;
+        char op = ' ';
+        int b = 0;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 3;
+            op = '+';
+            b = 7;
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+        }
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(op, a, b);
+        System.out.println(result);
+    }
 }
--- src/main/java/HW1_4/Printer.java
+++ src/main/java/HW1_4/Printer.java
@@ -1,2 +1,18 @@
-package HW1_4;public class Printer {
+package HW1_4;
+
+public class Printer {
+    public static void main(String[] args) {
+        String file = "";
+
+        if (args.length == 0) {
+            file = "input.txt";
+        }
+        else{
+            file = args[0];
+        }
+
+        Equation eq = new Equation();
+        String result = eq.getSolution(file);
+        System.out.println(result);
+    }
 }
